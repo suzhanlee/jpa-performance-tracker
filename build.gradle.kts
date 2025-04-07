382e1537413b5d1fe35bc5e2378fb46dafe10b29
suzhanlee <wlscww@kakao.com>
2025-04-07 22:20:27 +0900

    settings : 개발 환경 구성을 위한 기본 gradle 설정

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.example"
+version = "0.1.0-SNAPSHOT"
+description = "JPA Performance Tracker"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    implementation("org.hibernate.orm:hibernate-core")
+    implementation("org.slf4j:slf4j-api")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
